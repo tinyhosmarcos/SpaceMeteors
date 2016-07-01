7b042f23ba718c5ede4766a99b88ed06edf95fd8
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2016-06-30 23:23:27 -0500

    Sounds Effect
    Add Sounds

--- SpaceMeteors/arma.h
+++ SpaceMeteors/arma.h
@@ -2,10 +2,11 @@
 #define ARMA_H_INCLUDED
 #include <SDL.h>
 #include "SDL_Create.h"
-
+#include <SDL_mixer.h>
 class Weapons:public SDL_Create{
 private:
     const char* file="bullet.png";
+    Mix_Chunk *sound=Mix_LoadWAV("shoot.wav");
 public:
     Weapons(SDL_Renderer* temp_renderer,int post_x,int post_y);
     void init_movement();
